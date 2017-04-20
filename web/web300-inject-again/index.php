dfe79101d1128257568fae03368f62f5bd3502e8
WangYihang <wangyihanger@gmail.com>
2017-04-20 16:42:18 +0800

    web300-inject-again

---
+++ web/web300-inject-again/index.php
@@ -0,0 +1,52 @@
+<?php
+ini_set("display_errors", "On");
+error_reporting(E_ALL | E_STRICT);
+$dbhost = "localhost";
+$dbuser = "root";
+$dbpass = "";
+$db = "SniperOJ";
+$conn = mysqli_connect($dbhost,$dbuser,$dbpass,$db);
+mysqli_set_charset($conn,"utf8");
+
+function   filter($str){
+	$filterlist = "/\(|\)|username|password|where|case|when|like|regexp|into|limit|=|for|;/";
+	if(preg_match($filterlist,strtolower($str))){
+		die("Go away!");
+	}
+	return $str;
+}
+
+$username = isset($_GET['username'])?filter($_GET['username']):die("Please input username!");
+$password = isset($_GET['password'])?filter($_GET['password']):die("Please input password!");
+
+$sql = "select * from admin where  username ='$username' and password = '$password'";
+
+$res = $conn -> query($sql);
+
+if($res->num_rows>0){
+	$row = $res -> fetch_assoc();
+	if($row['id']){
+		echo $row['username'];
+	}
+}else{
+	echo "The content in the password column is the flag!";
+}
+
+?>
+
+
+<?php
+/*  
+    CREATE TABLE `users` (
+    `id` int NOT NULL AUTO_INCREMENT,
+    `username` varchar(16) NOT NULL,
+    `password` varchar(32) NOT NULL,
+    PRIMARY KEY (`id`)
+    ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+ */
+
+/*
+   INSERT INTO `users` (username, password) values ('admin','SniperOJ{BinD_injEct1oN}');
+
+ */
+?>
