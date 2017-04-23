0f4ad95b22f9d931a09617f2d932365e7ee870c6
WangYihang <wangyihanger@gmail.com>
2017-04-23 22:18:04 +0800

    fix a bug

--- misc/misc100-md5-collision/index.php
+++ misc/misc100-md5-collision/index.php
@@ -1,8 +1,8 @@
 <?php
 	$flag = 'SniperOJ{MD5_is_the_m0st_pow3r4_encryption_algorithm_all_over_the_world}';
 	if(isset($_POST['password']) && isset($_POST['username'])){
-		$username = $_POST['password'];
-		$password = $_POST['username'];
+		$username = strval($_POST['password']);
+		$password = strval($_POST['username']);
 		if ((md5($username) === md5($password)) && ($username != $password)){
 			echo $flag;
 		}else{
@@ -115,3 +115,4 @@
 			echo $flag;
 		}
 -->
+
